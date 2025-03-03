4af5f12cb624a90a5ed3b1d5022ccf09f66c03d5
sonulohani <sonulohani@gmail.com>
2025-03-03 09:40:01 +0530

    Fix fragment shader for camera

--- 06_virtual_camera/shaders/fragmentShader.glsl
+++ 06_virtual_camera/shaders/fragmentShader.glsl
@@ -3,10 +3,9 @@ out vec4 FragColor;
 in vec2 TexCoord;
 
 uniform sampler2D myTexture1;
-uniform sampler2D myTexture2;
 
 void main()
 {
-   FragColor = mix(texture(myTexture1, TexCoord), texture(myTexture2, TexCoord), 0.5);
+   FragColor = texture(myTexture1, TexCoord);
 }
 
